26ea96589843f306923c7dd9bb7417a8e5b42990
jhoncarlos79 <jhoncarlos@gmail.com>
2024-11-06 16:38:05 -0500

    Agregamos permiso para utilizar POST y DELETE

--- deportes/api/permissions.py
+++ deportes/api/permissions.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import BasePermission
 
 class IsAdminReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.method=='GET':            
+        if request.method=='GET' or request.method=='POST' or request.method=="DELETE":            
             return True
         else:            
             return request.user.is_staff and request.user.is_authenticated
