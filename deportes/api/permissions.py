ed4b794875fa1ca86988bb595ed8599f4261b1ae
jhoncarlos79 <jhoncarlos@gmail.com>
2024-11-07 19:06:41 -0500

    Quitamos permisos para probar API, en el front

--- deportes/api/permissions.py
+++ deportes/api/permissions.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import BasePermission
 
 class IsAdminReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.method=='GET' or request.method=='POST' or request.method=="DELETE":            
+        if request.method=='GET' or request.method=='POST' or request.method=="DELETE" or request.method=='PUT':            
             return True
         else:            
             return request.user.is_staff and request.user.is_authenticated
